3b53fb0b216c0291ecce8adcd6b6b1f9be55e9bc
Caner Tüysüz <canertuysuz@Caners-MacBook-Pro.local>
2023-08-12 18:52:12 +0300

    delete user feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import Table from "./Table.jsx";
 import { users } from "./users.js";
+import { useState } from "react";
+
+// TODO, Edit user
+// TODO, Add user(form built with formik)
+// TODO, copy email when clicked
 
 function App() {
+
+    const [user, setUsers] = useState(users)
+
     return (
       <div className="p-4">
           <Table
@@ -11,15 +19,20 @@ function App() {
                   {title: "age", sortable: true},
                   {title: "actions", width: 150}
               ]}
-              body={users.map(user => ([
-                  user.fullName,
-                  user.email,
-                  user.age,
+              body={user.map((u, key) => ([
+                  u.fullName,
+                  u.email,
+                  u.age,
                   [
                       <button className="w-14 p-1 bg-amber-500 rounded text-black">edit</button>,
-                      <button className="w-14 p-1 bg-red-500 rounded ml-2">delete</button>
+                      <button className="w-14 p-1 bg-red-500 rounded ml-2" onClick={() => {
+                          const tmpUsers = [...user]
+                          tmpUsers.splice(key, 1)
+                          setUsers(tmpUsers)
+                      }}>delete</button>
                   ]
               ]))} />
+          {user.length === 0 && <div className="text-black p-2 bg-amber-600 rounded mt-4">no users found</div>}
       </div>
   )
 }
