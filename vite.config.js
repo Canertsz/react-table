10b959c823b06364850b3b980b923bf77b2e57ce
Caner Tüysüz <canertuysuz@Caners-MBP.home>
2023-08-18 11:38:48 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-icons/Bs',
-        'react-icons/cg',
-        'react-icons/Fa',
-        'react-icons/si',
-      ], // Add this line
-    },
-  },
-});
+})
